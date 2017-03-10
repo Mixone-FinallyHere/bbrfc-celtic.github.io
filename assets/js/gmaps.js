77f367774640d38be7afce1941794e3a8c0234d2
mail@crequena.me <mail@crequena.me>
2017-03-10 10:38:08 +0000

    Automated commit at 2017-03-10 10:38:06 UTC by middleman-deploy 1.0.0

---
+++ assets/js/gmaps.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var e="https://www.google.com/maps/embed/v1/place?q=BBRFC+Celtic+Rugby+Club,+Boulevard+du+Triomphe,+Ixelles,+Belgium&key=AIzaSyBU2T9PaAWLTUyi9N_nKeKaJ8QUcxC85ZI";$("iframe#pitch").attr("src",e)});
